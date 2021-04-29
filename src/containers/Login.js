2a66e3a7ebda9e1e96afb986756d5ca418bd270e
Suyash <suyash.krishna9@gmail>
2021-04-29 19:03:27 +0400

    Remove API calls

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,76 +1,52 @@
 import React, { useState } from 'react';
-import getRequest from '../constants';
+import query from '../query';
 import '../assets/css/Login.scss';
 
 const Login = () => {
   const [signin, setSignin] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
-  const login = (e) => {
+  const performRequest = (e, url) => {
     e.preventDefault();
-    fetch(getRequest('POST', 'users/login', {
+    query('POST', url, {
       name: e.target.name.value,
       password: e.target.password.value,
-    }))
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((response) => {
-        if (response.token) {
-          localStorage.token = response.token;
-          window.location.reload();
-        }
-      })
-      .catch(() => {
-        setErrorMessage("Couldn't Sign In!");
-      });
-  };
-  const signup = (e) => {
-    e.preventDefault();
-    fetch(getRequest('POST', 'users/create', {
-      name: e.target.name.value,
-      password: e.target.password.value,
-    }))
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((response) => {
-        if (response.token) {
-          localStorage.token = response.token;
-          window.location.reload();
-        }
-      })
-      .catch(() => {
-        setErrorMessage("Couldn't Sign Up!");
-      });
+    }, (result) => {
+      if (result.token) {
+        localStorage.token = result.token;
+        window.location.reload();
+      } else {
+        setErrorMessage(result.error);
+      }
+    });
   };
   return (
     <div className="login">
-      {signin
-        ? (
-          <div className="col cross-center main-center">
-            <form onSubmit={login} className="col cross-center">
-              <h2>Sign In</h2>
-              <input type="text" name="name" placeholder="Username" />
-              <input type="password" name="password" placeholder="Password" />
-              <input type="submit" value="Sign In" className="link w-90" />
-              <button type="button" onClick={() => { setSignin(false); }} className="link button w-90">or create new account</button>
-            </form>
-          </div>
-        )
-        : (
-          <div className="col cross-center main-center">
-            <form onSubmit={signup} className="col cross-center">
-              <h2>Create new account</h2>
-              <input type="text" name="name" placeholder="Username" />
-              <input type="password" name="password" placeholder="Password" />
-              <input type="submit" value="Sign Up" className="link w-90" />
-              <button type="button" onClick={() => { setSignin(true); }} className="link button w-90">Go to Sign In page</button>
-            </form>
-          </div>
-        )}
-      <div className="error row main-center cross-center">{errorMessage}</div>
+      <div>
+        {signin
+          ? (
+            <div className="col cross-center main-center">
+              <form onSubmit={(e) => { performRequest(e, 'sessions'); }} className="col cross-center">
+                <h2>Sign In</h2>
+                <input type="text" name="name" placeholder="Username" />
+                <input type="password" name="password" placeholder="Password" />
+                <input type="submit" value="Sign In" className="link w-90" />
+                <button type="button" onClick={() => { setSignin(false); }} className="link button w-90">or create new account</button>
+              </form>
+            </div>
+          )
+          : (
+            <div className="col cross-center main-center">
+              <form onSubmit={(e) => { performRequest(e, 'users'); }} className="col cross-center">
+                <h2>Create new account</h2>
+                <input type="text" name="name" placeholder="Username" />
+                <input type="password" name="password" placeholder="Password" />
+                <input type="submit" value="Sign Up" className="link w-90" />
+                <button type="button" onClick={() => { setSignin(true); }} className="link button w-90">Go to Sign In page</button>
+              </form>
+            </div>
+          )}
+        <div className="error row main-center cross-center">{errorMessage}</div>
+      </div>
     </div>
   );
 };
