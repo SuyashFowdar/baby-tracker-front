a5aeafdd51d0c39461d852aeae7e7dd91aa436b3
Suyash <suyash.krishna9@gmail>
2021-04-29 19:00:20 +0400

    Update measurement to measure

--- src/components/HomeMeasurement.js
+++ src/components/HomeMeasurement.js
@@ -3,37 +3,41 @@ import PropTypes from 'prop-types';
 import { HashRouter as Router, Link } from 'react-router-dom';
 import '../assets/css/HomeMeasurement.scss';
 
-const HomeMeasurement = ({ measurement }) => (
-  <Router>
-    <Link to={`/measure/${measurement.id}`} className="measurement link bg-cover col cross-center main-center">
-      {measurement.list && measurement.list.length
-        ? (
-          <div>
-            <div className="amount">{`${measurement.list[measurement.list.length - 1].amount} ${measurement.unit}`}</div>
-            {measurement.list[measurement.list.length - 2]
-              ? (
-                <div className="row main-center dif" style={{ color: measurement.list[measurement.list.length - 1].amount > measurement.list[measurement.list.length - 2].amount ? 'green' : 'red' }}>
-                  {measurement.list[measurement.list.length - 1].amount
-                  > measurement.list[measurement.list.length - 2].amount ? '+' : ''}
-                  {(measurement.list[measurement.list.length - 1].amount
-                  - measurement.list[measurement.list.length - 2].amount).toFixed(2)}
-                </div>
-              )
-              : <div />}
-          </div>
-        )
-        : <div className="amount new">Add new</div>}
-      <div className="type">{measurement.item}</div>
-    </Link>
-  </Router>
-);
+const HomeMeasurement = ({ measure }) => {
+  const { measurements } = measure;
+
+  return (
+    <Router>
+      <Link to={`/measure/${measure.id}`} className="measure link bg-cover col cross-center main-center">
+        {measurements && measurements.length
+          ? (
+            <div>
+              <div className="amount">{`${measurements[measurements.length - 1].amount} ${measure.unit}`}</div>
+              {measurements[measurements.length - 2]
+                ? (
+                  <div className="row main-center dif" style={{ color: measurements[measurements.length - 1].amount > measurements[measurements.length - 2].amount ? 'green' : 'red' }}>
+                    {measurements[measurements.length - 1].amount
+                    > measurements[measurements.length - 2].amount ? '+' : ''}
+                    {(measurements[measurements.length - 1].amount
+                    - measurements[measurements.length - 2].amount).toFixed(2)}
+                  </div>
+                )
+                : <div />}
+            </div>
+          )
+          : <div className="amount new">Add new</div>}
+        <div className="type">{measure.item}</div>
+      </Link>
+    </Router>
+  );
+};
 
 HomeMeasurement.defaultProps = {
-  measurement: {},
+  measure: {},
 };
 
 HomeMeasurement.propTypes = {
-  measurement: PropTypes.instanceOf(Object),
+  measure: PropTypes.instanceOf(Object),
 };
 
 export default HomeMeasurement;
