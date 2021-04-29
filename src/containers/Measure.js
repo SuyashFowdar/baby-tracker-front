f292caeb5275d934246ec2754cf800b7d0350632
Suyash <suyash.krishna9@gmail>
2021-04-29 19:03:58 +0400

    Remove API calls + update naming

--- src/containers/Measure.js
+++ src/containers/Measure.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import getRequest from '../constants';
+import query from '../query';
 import List from '../components/List';
 import { addMeasurement } from '../actions';
 
@@ -10,38 +10,30 @@ const PetDetail = () => {
   const { id } = useParams();
   const [measure, setMeasure] = useState({});
   const [errorMessage, setErrorMessage] = useState('');
-  const measurements = useSelector((state) => state.measurements);
+  const measures = useSelector((state) => state.measures);
   const selectMeasure = () => {
-    for (let i = 0; i < measurements.length; i += 1) {
-      if (measurements[i].id === parseInt(id, 10)) {
-        setMeasure(measurements[i]);
+    for (let i = 0; i < measures.length; i += 1) {
+      if (measures[i].id === parseInt(id, 10)) {
+        setMeasure(measures[i]);
         break;
       }
     }
   };
   const createMeasurement = (e) => {
     e.preventDefault();
-    let measurement = {
+    query('POST', 'measurements', {
       amount: e.target.amount.value,
       measure_id: measure.id,
-    };
-    fetch(getRequest('POST', 'measurements/create', measurement))
-      .then((response) => {
-        setErrorMessage('');
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((response) => {
-        if (response.measurement) {
-          measurement = response.measurement;
-          e.target.amount.value = '';
-          dispatch(addMeasurement(measure.id, measurement));
-          window.location.reload();
-        }
-      })
-      .catch(() => {
-        setErrorMessage("Couldn't add Measurement!");
-      });
+    }, (result) => {
+      if (result.measurement) {
+        e.target.amount.value = '';
+        const { measurement } = result;
+        measurement.measure_id = measure.id;
+        dispatch(addMeasurement(measurement));
+      } else {
+        setErrorMessage(result.error);
+      }
+    });
   };
   useEffect(() => {
     selectMeasure();
@@ -50,7 +42,7 @@ const PetDetail = () => {
     <>
       <div className="row cross-center main-center">
         <div className="w-100 h-100">
-          <List list={measure.list} itemKey="created_at" displayAttr={['date', 'amount']} toAddAttr={['amount']} unit={measure.unit} addItem={createMeasurement} />
+          <List list={measure.measurements} itemKey="created_at" displayAttr={['date', 'amount']} toAddAttr={['amount']} unit={measure.unit} addItem={createMeasurement} />
           <div className="error row main-center cross-center">{errorMessage}</div>
         </div>
       </div>
